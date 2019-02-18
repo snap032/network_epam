fb3338b3ed10894d67e5cf16696e0608e80a9177
snap032 <44320435+snap032@users.noreply.github.com>
2019-02-18 14:17:29 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,14 +4,14 @@ sudo apt-get update
 sudo tee -a /etc/network/interfaces << EOF
 auto enp0s8
 iface enp0s8 inet static
-address 122.168.3.5
+address 192.168.3.5
 netmask 255.255.255.192
 EOF
 
 # DHCP Server 
 sudo apt-get -y install isc-dhcp-server
 sudo apt-get install git -y
-git https://github.com/snap032/network_epam.git
+git clone https://github.com/snap032/network_epam.git
 mv network_epam/dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo sed -i 's/INTERFACES=""/INTERFACES="enp0s8"/' /etc/default/isc-dhcp-server
 sudo service isc-dhcp-server restart
